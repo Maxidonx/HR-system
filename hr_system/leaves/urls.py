b8667b604ebe55e7e8ca16ce31986e61b8ab530f
Maxidonx <johnmaxwell927@gmail.com>
2025-06-09 13:15:54 +0100

    Updated the router

--- hr_system/leaves/urls.py
+++ hr_system/leaves/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import LeaveRequestViewSet
 
 router = DefaultRouter()
-router.register(r'leaves', LeaveRequestViewSet, basename='leave')
+router.register(r'', LeaveRequestViewSet, basename='leaves')
 
 urlpatterns = [
     path('', include(router.urls)),
